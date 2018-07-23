9e5215d6bbbc575ed0043d5d57e9c7393d01379c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-07-23 23:09:30 +0200

    Add prod config

--- config/prod.exs
+++ config/prod.exs
@@ -1,4 +1,28 @@
 use Mix.Config
 
 config :hexdocs,
-  hexpm_url: "https://hex.pm"
+  port: {:integer, "${HEXDOCS_PORT}"},
+  hexpm_url: "${HEXDOCS_HEXPM_URL}",
+  hexpm_secret: "${HEXDOCS_HEXPM_SECRET}",
+  queue_name: "${HEXDOCS_QUEUE_NAME}",
+  hexpm_impl: Hexpm.Hexpm.Impl,
+  store_impl: Hexpm.Store.Impl,
+  session_key_base: "${HEXDOCS_SESSION_KEY_BASE}",
+  session_signing_salt: "${HEXDOCS_SESSION_SIGNING_SALT}",
+  session_encryption_salt: "${HEXDOCS_SESSION_ENCRYPTION_SALT}",
+  host: "${HEXDOCS_HOST}"
+
+config :hexdocs, :repo_bucket,
+  name: "${HEXDOCS_REPO_BUCKET}",
+  implementation: Hexpm.Store.S3
+
+config :hexdocs, :docs_bucket,
+  name: "${HEXDOCS_DOCS_BUCKET}",
+  implementation: Hexpm.Store.GS
+
+config :ex_aws,
+  access_key_id: "${HEXDOCS_AWS_ACCESS_KEY_ID}",
+  secret_access_key: "${HEXDOCS_AWS_SECRET_ACCESS_KEY}",
+  json_codec: Jason
+
+config :goth, json: "${HEXDOCS_GCP_CREDENTIALS}"
