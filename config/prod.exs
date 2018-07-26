5aca2650d753a17a60a8e780c74ae05dffddaf67
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-07-26 11:53:49 +0200

    Fix store config

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ config :hexdocs,
   hexpm_secret: "${HEXDOCS_HEXPM_SECRET}",
   queue_name: "${HEXDOCS_QUEUE_NAME}",
   hexpm_impl: Hexpm.Hexpm.Impl,
-  store_impl: Hexpm.Store.Impl,
+  store_impl: Hexdocs.Store.Impl,
   session_key_base: "${HEXDOCS_SESSION_KEY_BASE}",
   session_signing_salt: "${HEXDOCS_SESSION_SIGNING_SALT}",
   session_encryption_salt: "${HEXDOCS_SESSION_ENCRYPTION_SALT}",
@@ -14,11 +14,11 @@ config :hexdocs,
 
 config :hexdocs, :repo_bucket,
   name: "${HEXDOCS_REPO_BUCKET}",
-  implementation: Hexpm.Store.S3
+  implementation: Hexdocs.Store.S3
 
 config :hexdocs, :docs_bucket,
   name: "${HEXDOCS_DOCS_BUCKET}",
-  implementation: Hexpm.Store.GS
+  implementation: Hexdocs.Store.GS
 
 config :ex_aws,
   access_key_id: "${HEXDOCS_AWS_ACCESS_KEY_ID}",
