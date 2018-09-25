314340cf060a4d23ecedcb64d6c02f107a456eaf
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-09-25 22:59:49 +0200

    Add runtime_tools to extra_applications

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Hexdocs.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools],
       mod: {Hexdocs.Application, []}
     ]
   end
