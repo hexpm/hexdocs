7c2e883ac26148099a485ed5718acc4fcd63cd7c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-06-13 21:15:05 +0200

    Include eex in release

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Hexdocs.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :runtime_tools, :plug_crypto],
+      extra_applications: [:eex, :logger, :runtime_tools, :plug_crypto],
       mod: {Hexdocs.Application, []}
     ]
   end
