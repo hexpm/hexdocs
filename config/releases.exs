a3ef7dd57ceb42ad445c29cbff75a8b9f95d1569
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-10-16 01:01:56 +0200

    Add BEAM_PORT config

--- config/releases.exs
+++ config/releases.exs
@@ -26,3 +26,7 @@ config :goth, json: System.fetch_env!("HEXDOCS_GCP_CREDENTIALS")
 
 config :rollbax,
   access_token: System.fetch_env!("HEXDOCS_ROLLBAR_ACCESS_TOKEN")
+
+config :kernel,
+  inet_dist_listen_min: String.to_integer(System.fetch_env!("BEAM_PORT")),
+  inet_dist_listen_max: String.to_integer(System.fetch_env!("BEAM_PORT"))
