985b35211f7f4e98efc7b6da2bed31d52a9611fd
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-05-03 20:40:42 +0200

    Multi purge

--- lib/hexdocs/cdn/fastly.ex
+++ lib/hexdocs/cdn/fastly.ex
@@ -1,13 +1,23 @@
 defmodule Hexdocs.CDN.Fastly do
   @behaviour Hexdocs.CDN
   @fastly_url "https://api.fastly.com/"
+  @fastly_purge_wait 4000
 
   def purge_key(service, keys) do
     keys = keys |> List.wrap() |> Enum.uniq()
     body = %{"surrogate_keys" => keys}
     service_id = Application.get_env(:hexdocs, service)
+    sleep_time = div(Application.get_env(:hexdocs, :fastly_purge_wait, @fastly_purge_wait), 2)
 
     {:ok, 200, _, _} = post("service/#{service_id}/purge", body)
+
+    Task.Supervisor.start_child(Hexdocs.Tasks, fn ->
+      Process.sleep(sleep_time)
+      {:ok, 200, _, _} = post("service/#{service_id}/purge", body)
+      Process.sleep(sleep_time)
+      {:ok, 200, _, _} = post("service/#{service_id}/purge", body)
+    end)
+
     :ok
   end
 
