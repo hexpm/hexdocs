43b2b0f392d9c81d4be6298f708403b24090cd3d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2025-01-12 18:50:03 +0100

    Build docker images on github actions (#52)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hexdocs.MixProject do
     [
       app: :hexdocs,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       releases: releases(),
       deps: deps()
